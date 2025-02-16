4d029f5ed6ae2f78fbb6e61077fcf35092d1e1d5
batman <teodorgeorgiev@proton.me>
2025-02-17 00:05:56 +0200

    Update text-to-speach config

--- livekit_playground/agent/tts.py
+++ livekit_playground/agent/tts.py
@@ -4,10 +4,18 @@ Text-to-speach configuration
 import os
 from dotenv import load_dotenv
 from livekit.plugins import elevenlabs
+from livekit.plugins.elevenlabs import Voice
 
 load_dotenv()
+
+custom_voice = Voice(
+    id="fSxb5mPM1l5zTVVtM3Vb",
+    name="Elena",
+    category='custom'
+)
 
 eleven_tts = elevenlabs.TTS(
     model="eleven_turbo_v2_5",
     api_key=os.getenv("ELEVEN_LABS_API_KEY"),
+    voice=custom_voice
 )
